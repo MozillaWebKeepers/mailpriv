44d2a0812c581d3794d7d7d3a8a86e146617e49b
kanikaa1234 <murarkakanika@gmail.com>
2016-06-04 16:04:06 +0530

    Toolbar button added

--- index.js
+++ index.js
@@ -6,4 +6,22 @@ function dummy(text, callback) {
   callback(text);
 }
 
+var buttons = require('sdk/ui/button/action');
+var tabs = require("sdk/tabs");
+
+var button = buttons.ActionButton({
+  id: "mozilla-link",
+  label: "Visit Mozilla",
+  icon: {
+    "16": "./icon-16.png",
+    "32": "./icon-32.png",
+    "64": "./icon-64.png"
+  },
+  onClick: handleClick
+});
+
+function handleClick(state) {
+  tabs.open("http://www.saurabhbadhwar.xyz/");
+}
+
 exports.dummy = dummy;
